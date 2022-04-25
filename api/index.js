94423137d8ceec2a2dbf2f00caaff7bc667a45b5
Mariama Ali <mariama@Mariamas-MBP.ht.home>
2022-04-24 21:53:25 -0400

    login and register routes

--- api/index.js
+++ api/index.js
@@ -5,13 +5,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
-
+//body-parser
 app.use(bodyParser.json())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
     extended:true
 }))
 
+//database
 mongoose.connect(process.env.DB,{
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -22,9 +23,11 @@ var db = mongoose.connection;
 db.on('error',()=>console.log("Error in Connecting to Database"));
 db.once('open',()=>console.log("Connected to Database"))
 
-
+//routes
+app.use("/", require("./routes/auth"));
 app.use('*', cors());
 
+//port
 const port = process.env.PORT || 4000
 
 app.listen(port, ()=>{
